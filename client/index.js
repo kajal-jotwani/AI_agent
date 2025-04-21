571bcc71926d1dd228f703b575f7330312f0cf3b
kajal-jotwani <kajaljotwani06@gmail.com>
2025-04-21 10:12:55 +0530

    Updated the mcp tools with tweet tool

--- client/index.js
+++ client/index.js
@@ -1,30 +1,31 @@
-import {config} from 'dotenv';
-import readline from 'readline/promises';
-import { GoogleGenAI } from "@google/genai";
+import { config } from 'dotenv';
+import readline from 'readline/promises'
+import { GoogleGenAI } from "@google/genai"
 import { Client } from "@modelcontextprotocol/sdk/client/index.js"
-import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js"
 
-config();
 
+config()
 let tools = []
-
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 const mcpClient = new Client({
-    name: 'example-client',
-    version: '1.0.0',
-});
+    name: "example-client",
+    version: "1.0.0",
+})
+
+
 
 const chatHistory = [];
-//creating an instance of readline
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-})
+});
+
 
 mcpClient.connect(new SSEClientTransport(new URL("http://localhost:3001/sse")))
     .then(async () => {
 
-        console.log("Connected to mcp server");
+        console.log("Connected to mcp server")
 
         tools = (await mcpClient.listTools()).tools.map(tool => {
             return {
@@ -36,78 +37,89 @@ mcpClient.connect(new SSEClientTransport(new URL("http://localhost:3001/sse")))
                     required: tool.inputSchema.required
                 }
             }
-        });   
-    chatLoop()
+        })
+
+        chatLoop()
+
+
     })
 
 async function chatLoop(toolCall) {
-    if(toolCall){
-        
+
+    if (toolCall) {
+
+        console.log("calling tool ", toolCall.name)
+
         chatHistory.push({
-            role: 'model',
+            role: "model",
             parts: [
                 {
                     text: `calling tool ${toolCall.name}`,
-                    type: 'text'
+                    type: "text"
                 }
             ]
         })
 
-       const toolResult = await  mcpClient.callTool({
+        const toolResult = await mcpClient.callTool({
             name: toolCall.name,
             arguments: toolCall.args
         })
 
         chatHistory.push({
-            role: 'user',
+            role: "user",
             parts: [
                 {
-                    text: "Tool result" + toolResult.content[0].text,
-                    type: 'text'
+                    text: "Tool result : " + toolResult.content[ 0 ].text,
+                    type: "text"
                 }
             ]
         })
-    }else{
 
-    const question = await rl.question('You: ')
+    } else {
+        const question = await rl.question('You: ');
+        chatHistory.push({
+            role: "user",
+            parts: [
+                {
+                    text: question,
+                    type: "text"
+                }
+            ]
+        })
+    }
 
-    chatHistory.push({
-        role: 'user',
-        parts: [
-            {
-                text: question,
-                type: 'text'
-            }
-        ]
-    })
-}
     const response = await ai.models.generateContent({
-        model: 'gemini-1.5-flash', // 1.5 has better tool support
+        model: "gemini-2.0-flash",
         contents: chatHistory,
-        tools: [
-            {
-                functionDeclarations: tools
-            }
-        ]
-    });
-    
+        config: {
+            tools: [
+                {
+                    functionDeclarations: tools,
+                }
+            ]
+        }
+    })
+    const functionCall = response.candidates[ 0 ].content.parts[ 0 ].functionCall
+    const responseText = response.candidates[ 0 ].content.parts[ 0 ].text
 
-    const functionCall = response.candidates[0].content.parts[0].functionCall
-    const responseText = response.candidates[0].content.parts[0].text
-
-    if(functionCall){
+    if (functionCall) {
         return chatLoop(functionCall)
     }
 
+
     chatHistory.push({
-        role: 'model',
+        role: "model",
         parts: [
             {
                 text: responseText,
-                type: 'text'
+                type: "text"
             }
         ]
     })
+
     console.log(`AI: ${responseText}`)
+
+
     chatLoop()
+
 }
